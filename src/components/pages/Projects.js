c4c10e2c8a0d0b3758dc9b6bade12722c5a59234
Daniel Dereniowski <leinad885@o2.pl>
2021-07-18 21:21:35 +0200

    Switched projects <-> tasks pages (silly error)

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,28 +1,10 @@
 import classes from '../../styles/Projects.module.scss';
-import TaskItem from '../TaskItem';
 
 function Projects() {
     return (
-        <main className={classes.content}>
-            <TaskItem 
-                projectName="Project 1"
-                taskPriority="High"
-                taskName="Implement mobile version of pdp"
-                taskDescription="Please do it very very fast."
-            />
-            <TaskItem 
-                projectName="Project 2"
-                taskPriority="Medium"
-                taskName="Restyle the button"
-                taskDescription="Lubie Placki z dzemorem"
-            />
-            <TaskItem 
-                projectName="Project 3"
-                taskPriority="Low"
-                taskName="Change bgcolor"
-                taskDescription="XDxdxDXD"
-            />
-        </main>
+        <div>
+
+        </div>
     );
 }
 
--- src/components/pages/Tasks.js
+++ src/components/pages/Tasks.js
@@ -1,8 +1,28 @@
+import classes from '../../styles/Tasks.module.scss';
+import TaskItem from '../TaskItem';
+
 function Tasks() {
     return (
-        <div>
-            
-        </div>
+        <main className={classes.content}>
+            <TaskItem 
+                projectName="Project 1"
+                taskPriority="High"
+                taskName="Implement mobile version of pdp"
+                taskDescription="Please do it very very fast."
+            />
+            <TaskItem 
+                projectName="Project 2"
+                taskPriority="Medium"
+                taskName="Restyle the button"
+                taskDescription="Lubie Placki z dzemorem"
+            />
+            <TaskItem 
+                projectName="Project 3"
+                taskPriority="Low"
+                taskName="Change bgcolor"
+                taskDescription="XDxdxDXD"
+            />
+        </main>
     );
 }
 
