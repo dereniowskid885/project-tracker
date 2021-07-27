417bd80b0f37a889e576915a761096813d5010be
Daniel Dereniowski <leinad885@o2.pl>
2021-07-27 19:50:28 +0200

    Project details show tasks

--- src/components/DetailsWindow/ProjectDetailsWindow.js
+++ src/components/DetailsWindow/ProjectDetailsWindow.js
@@ -1,15 +1,38 @@
 import classes from '../../styles/DetailsWindow.module.scss';
+import Tasks from '../pages/Tasks';
+import { useState } from 'react';
 
 function ProjectDetailsWindow(props) {
+    const [ tasksAreOpen, setTasksState ] = useState(false);
+
+    function showTasks() {
+        setTasksState(true);
+    }
+
+    function hideTasks() {
+        setTasksState(false);
+    }
+
     return (
-        <div className={classes.window}>
+        <div className={classes.window + ' ' + [tasksAreOpen && classes.window__tasksDetails]}>
             <h1>{props.projectName}</h1>
             <ul>
                 <li>XD</li>
                 <li>XDXDXD</li>
             </ul>
             <p>{props.projectDescription}</p>
-            <button className={classes.window__btn} onClick={props.onCloseBtnClick}>Close</button>
+            { tasksAreOpen &&
+                <div>
+                    <Tasks />
+                </div>
+            }
+            { tasksAreOpen &&
+                <button className={classes.window__btn + ' ' + classes.window__btnDouble} onClick={hideTasks}>Hide Tasks</button>
+            }
+            { tasksAreOpen ||
+                <button className={classes.window__btn + ' ' + classes.window__btnDouble} onClick={showTasks}>Show Tasks</button>
+            }
+            <button className={classes.window__btn + ' ' + classes.window__btnDouble} onClick={props.onCloseBtnClick}>Close</button>
         </div>
     );
 }
